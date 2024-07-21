d17eb88f5f3ecbf3b0f6f39682017cb2b6374fd0
Kinotable <prof.darshdeep@gmail.com>
2024-07-21 20:18:20 +0400

    evenorodd

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,21 @@ public class Main {
         String Msg = (x >= 50) ? "gets to go to picnic" : "no picnic";
         return Msg;
     }
+    // even or odd
+    public static String EvenOrOdd(int num){
+        String Msg = ( num % 2 == 0) ? "is even" : "is odd";
+        return Msg;
+    }
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("pls enter the marks");
         int marks = s.nextInt();
         String msg = Marks(marks);
         System.out.println(msg);
-
+//even or odd
+        System.out.println("pls enter the Even or odd");
+        int num = s.nextInt();
+        String evenorodd = EvenOrOdd(num);
+        System.out.println(evenorodd);
     }
 }
